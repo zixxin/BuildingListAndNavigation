8dafca0e0d0e1d870249e459481ceb5a71e0da20
zixxin <74345861+zixxin@users.noreply.github.com>
2023-01-16 12:25:02 +0900

    Initial Commit

---
+++ BuildingListAndNavigation/BuildingListAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListAndNavigationApp.swift
+//  BuildingListAndNavigation
+//
+//  Created by 윤지인 on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct BuildingListAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
